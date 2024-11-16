45146f0f870bb4b07304f5c36cb7d9eec5634838
Steven <storrence88@gmail.com>
2024-11-15 23:58:35 -0600

    Update telescope to ignore node_modules by default

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -59,6 +59,9 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
+        file_ignore_patterns = {
+          "node_modules",
+        },
       },
     },
   },
